5091b2eff2e289355618ebbd32e8015f446dbb3a
Praveen <praveen.nitap@gmail.com>
2018-02-22 15:58:51 +0530

    Permission Related issue resolved and changed error messages

--- app/src/main/java/com/github/ele_sms/view/MainActivity.java
+++ app/src/main/java/com/github/ele_sms/view/MainActivity.java
@@ -2,7 +2,9 @@ package com.github.ele_sms.view;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -11,9 +13,11 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.github.ele_sms.MainActivityContract;
 import com.github.ele_sms.presenter.MainActivityPresenter;
@@ -53,28 +57,53 @@ public class MainActivity extends AppCompatActivity implements MainActivityContr
         mRecyclerView.setLayoutManager(layoutManager);
         mRecyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        checkAndRequestPermissions();
-        presenter.fetchData();
+        if (isPermissionGranted()) {
+            presenter.fetchData();
+        }
 
     }
 
 
-    private  boolean checkAndRequestPermissions()
-    {
-        int sms = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_SMS);
-        List<String> listPermissionsNeeded = new ArrayList<>();
+    public boolean isPermissionGranted() {
 
-        if (sms != PackageManager.PERMISSION_GRANTED)
-        {
-            listPermissionsNeeded.add(Manifest.permission.READ_SMS);
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (checkSelfPermission(Manifest.permission.READ_SMS)
+                    == PackageManager.PERMISSION_GRANTED ) {
+                Log.e(TAG, "Permission is granted");
+                return true;
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.READ_SMS
+                }, 1);
+                return false;
+            }
+        } else { //permission is automatically granted on sdk<23 upon installation
+            Log.e(TAG, "Permission is granted");
+            return true;
         }
-        if (!listPermissionsNeeded.isEmpty())
-        {
-            ActivityCompat.requestPermissions(this,listPermissionsNeeded.toArray(new
-                    String[listPermissionsNeeded.size()]),1);
-            return false;
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        if (requestCode == 1) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                // permission was granted, yay! Do the
+                // contacts-related task you need to do.
+                Toast.makeText(getApplicationContext(), "Permission granted", Toast.LENGTH_SHORT).show();
+                presenter.fetchData();
+
+            } else {
+
+                // permission denied, boo! Disable the
+                // functionality that depends on this permission.
+                Toast.makeText(getApplicationContext(), "Permission denied", Toast.LENGTH_SHORT).show();
+            }
         }
-        return true;
     }
 
     @Override
